fe26a6d919cccce6518a829389bdca959d1e4db1
Fredrik Boström <fredrik@fredrikbostrom.net>
2022-01-19 01:07:00 +0200

    chore(deps): remove unnecessary eslint-configs, update tsconfig base to node 12

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
-  extends: 'motley-typescript',
   env: {
     node: true,
     jest: true,
+  },
+  extends: [
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    curly: ['error', 'all'],
   },
 };
