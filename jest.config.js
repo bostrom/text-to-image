7a581cfe3b53e418b80e41d1069a39fea765f762
Fredrik Boström <fredrik@fredrikbostrom.net>
2025-08-12 23:10:06 +0300

    chore(deps): update dependencies
    Update all dependencies. Migrate config files to new formats. Use Node20 as TypeScript base config.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'node',
   preset: 'ts-jest',
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  testPathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['node_modules/', 'dist/'],
   coveragePathIgnorePatterns: [
-    '/node_modules/',
-    'test/helpers/*',
+    'node_modules/',
+    'src/tests',
+    'src/@types',
     'src/index.ts',
   ],
   coverageProvider: 'v8',
 };
+
+module.exports = config;
